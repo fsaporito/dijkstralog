9b472d15c41e69b41462536ef56ba2b72f8d6187
sapo93 <sapo93@gmail.com>
2013-11-15 13:59:27 +0100

    Create sssp.pl
    Main file that loads everything else

---
+++ sssp.pl
@@ -0,0 +1,22 @@
+% Saporito Francesco 763855
+
+
+%%%%  Graph Implementation  %%%%%
+:- consult('graph.pl').
+
+
+%%%%  MinHeap Implementation %%%%%
+:- consult('min_heap.pl').
+
+
+%%%%  Dijkstra's Algorithm Implementation %%%%%
+:- consult('dijkstra.pl').
+
+
+%%%%  Help File  %%%%
+:- consult('help.pl').
+
+
+
+%%%% Test File  %%%%
+:- consult('test.pl').
